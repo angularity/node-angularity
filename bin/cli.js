24cf7273541db5e6d97ee72f8b1df525efde7e98
Chris Decoster <chris.decoster@3plearning.com>
2014-10-29 16:07:02 +1100

    feature(generator) initial implementation of the generator with es5 hello world.

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,36 @@
 #!/usr/bin/env node
 
-'use strict';
-
-var gulp       = require('gulp');
-var gutil      = require('gulp-util');
-var prettyTime = require('gulp/node_modules/pretty-hrtime');
-var chalk      = require('gulp/node_modules/chalk');
+/**
+ * The main command line interface for running Angularity
+ * from the npm global install.
+ *
+ * If no arguments are specified it will load an interactive cli menu.
+ */
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var prettyTime = require('pretty-hrtime');
+var chalk = require('chalk');
 
 require('../index');
 
+var generator = require('../lib/generator/generator');
+generator.requireProjects();
+
 gulp.on('task_start', function (e) {
-  gutil.log('Starting', '\'' + chalk.cyan(e.task) + '\'...');
+    gutil.log('Starting', '\'' + chalk.cyan(e.task) + '\'...');
 });
 
 gulp.on('task_stop', function (e) {
-  var time = prettyTime(e.hrDuration);
-  gutil.log(
-    'Finished', '\'' + chalk.cyan(e.task) + '\'',
-    'after', chalk.magenta(time)
-  );
+    var time = prettyTime(e.hrDuration);
+    gutil.log(
+        'Finished', '\'' + chalk.cyan(e.task) + '\'',
+        'after', chalk.magenta(time)
+    );
 });
 
 var taskName = process.argv[2];
-gulp.start(gulp.hasTask(taskName) ? taskName : 'default');
+
+if (typeof taskName === 'undefined')
+    require('../lib/generator/cliMenu').defaultPrompt();
+else
+    gulp.start(gulp.hasTask(taskName) ? taskName : 'default');
