2315dda46b5fbd73d8e68807f4e101f65616e020
benholloway <ben.holloway@3plearning.com>
2015-01-06 14:58:21 +1100

    added server port to console output

--- tasks/server.js
+++ tasks/server.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
     browserSync = require('browser-sync');
 
 var angularity = require('../index');
 
 gulp.task('server', ['build'], function () {
   console.log(angularity.hr('-', angularity.CONSOLE_WIDTH, 'server'));
+  gutil.log('serving on port:', angularity.HTTP_PORT);
   browserSync({
     server  : {
       baseDir: angularity.HTML_BUILD,
@@ -20,5 +22,6 @@ gulp.task('server', ['build'], function () {
 
 gulp.task('reload', function () {
   console.log(angularity.hr('-', angularity.CONSOLE_WIDTH, 'reload'));
+  gutil.log('serving on port:', angularity.HTTP_PORT);
   browserSync.reload();
 });
