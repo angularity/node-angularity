20bf1d8361a8f483490f211a7e5499b1310d083d
Brendan Graetz <Brendan Graetz>
2015-02-03 18:22:48 +1100

    =BG= Render SCSS using custom template + use bguiz/css-sprite#absolute-path-fix which fixes a problem with absolute paths In addition, preliminary work for identifying sprite library and class names from HTML files - quick and dirty using regular expressions

--- tasks/sprite.js
+++ tasks/sprite.js
@@ -1,4 +1,5 @@
 'use strict';
+console.log('__dirname', __dirname);
 
 var path = require('path');
 
@@ -11,14 +12,42 @@ var projectOfFile = require('project-of-file');
 
 var configStreams = require('../lib/config/streams');
 
-gulp.task('identifysprites', [], function() {
-
-});
-
 var projectOfFileInstance = projectOfFile.cachedInstance('angularity-sprite', process.cwd());
+
+gulp.task('idsprite', [], function() {
+  var list = {}
+  return configStreams.imageHtmlApp()
+    .pipe(through.obj(function(file, encoding, done) {
+      if (!file || !file.path || !file.isBuffer()) {
+        throw 'Files must be buffers';
+      }
+      var contents = file.contents.toString();
+      var regex = /\<i\s[^\>]*class\=[\"\']sprite-[\w-]+--[\w-]+[\"\'][^\>]*\>/gim ;
+      var matches = contents.match(regex);
+      if (matches && matches.length > 0) {
+        matches = matches.map(function(tag) {
+          var info = (tag).match( /\<i\s[^\>]*class\=[\"\']sprite-([\w-]+)--([\w-]+)[\"\'][^\>]*\>/im );
+          return {
+            lib: info[1],
+            img: info[2],
+          };
+        })
+      }
+      else {
+        matches = [];
+      }
+      list[file.path] = matches;
+      done();
+    }))
+    .on('end', function() {
+      console.log('list', list);
+    });
+});
 
 gulp.task('sprite', [], function() {
   return configStreams.imageApp()
+    // .pipe(/*browserify get list of classes*/)
+    // .pipe(/*through to filter list of files based on stuff*/)
     // .pipe(gulpNewer(configStreams.BUILD + '/sprite/_sprite.scss'))
     .pipe(through.obj(function(file, encoding, done) {
       var stream = this;
@@ -45,6 +74,7 @@ gulp.task('sprite', [], function() {
         // "generate both retina and standard sprites. src images have to be in retina resolution"
         retina: true,
         orientation: 'binary-tree',
+        template: path.join(__dirname, 'sprites.scss.mustache'),
       })
     )
     .pipe(gulp.dest(configStreams.BUILD + '/sprite/'));
