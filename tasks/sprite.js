d7fd54e15141cd9ba31996a96b0bc8c1a3150ffa
Brendan Graetz <Brendan Graetz>
2015-02-04 15:33:35 +1100

    =BG= identify possible project and image names of sprites by looking for usages in HTML sources of app
    - much wrangling with gulp streams
    - NOTE disambiguation requires unique project names
    - NOTE investigate if there is a better way to look through HTML than regular expressions, such as a token parser

--- tasks/sprite.js
+++ tasks/sprite.js
@@ -1,11 +1,8 @@
 'use strict';
-console.log('__dirname', __dirname);
 
 var path = require('path');
 
 var gulp = require('gulp');
-var gulpIf = require('gulp-if');
-var gulpNewer = require('gulp-newer');
 var cssSprite = require('css-sprite');
 var through = require('through2');
 var projectOfFile = require('project-of-file');
@@ -14,14 +11,21 @@ var configStreams = require('../lib/config/streams');
 
 var projectOfFileInstance = projectOfFile.cachedInstance('angularity-sprite', process.cwd());
 
-gulp.task('idsprite', [], function() {
-  var list = {}
+gulp.task('sprite', [], function() {
+  var list = {};
+  var mainProjectName = projectOfFileInstance.name(path.join(process.cwd(), 'package.json'));
+
   return configStreams.imageHtmlApp()
-    .pipe(through.obj(function(file, encoding, done) {
+    .pipe(through.obj(function transformFn(file, encoding, done) {
       if (!file || !file.path || !file.isBuffer()) {
         throw 'Files must be buffers';
       }
+
+      //using regular expressions, identify possible sprite image file names,
+      //and the projects which they should belong to
+      //TODO investigate if a token-based parser would be a more apt solution
       var contents = file.contents.toString();
+      var match, matches = [];
       var regex = /\<i\s[^\>]*class\=[\"\']sprite-[\w-]+--[\w-]+[\"\'][^\>]*\>/gim ;
       var matches = contents.match(regex);
       if (matches && matches.length > 0) {
@@ -36,36 +40,58 @@ gulp.task('idsprite', [], function() {
       else {
         matches = [];
       }
-      list[file.path] = matches;
+      matches.forEach(function(match) {
+        var lib = list[match.lib];
+        if (!lib) {
+          lib = {};
+        }
+        lib[match.img] = true;
+        list[match.lib] = lib;
+      });
       done();
-    }))
-    .on('end', function() {
-      console.log('list', list);
-    });
-});
-
-gulp.task('sprite', [], function() {
-  return configStreams.imageApp()
-    // .pipe(/*browserify get list of classes*/)
-    // .pipe(/*through to filter list of files based on stuff*/)
-    // .pipe(gulpNewer(configStreams.BUILD + '/sprite/_sprite.scss'))
-    .pipe(through.obj(function(file, encoding, done) {
+    },
+    function flushFn(done) {
       var stream = this;
-      //Prefix the file name of all images with the project name so as to create
-      //disambiguation between sprites from mutliple dependencies.
-      //The need to manipulate file names is inherent from the spriting library
-      //using file names to generate CSS classes for each sprite.
-      var projectName = projectOfFileInstance.name(file.path);
-      file.path = path.join(
-          process.cwd(), 'sprite', projectName + '--' + path.basename(file.path));
-      stream.push(file);
-      done(/*err*/);
+      //create a very specific set of glob strings based on project and image names
+      //NOTE that should there be an overlap (e.g. when there are multiple dependencies with the same name)
+      //the behaviour is indeterminate
+      var mainGlob = [];
+      for (var libName in list) {
+        if (list.hasOwnProperty(libName)) {
+          var lib = list[libName];
+          for (var imgName in lib) {
+            if (lib.hasOwnProperty(imgName)) {
+              if (libName === mainProjectName) {
+                mainGlob.push('**!(node_modules|bower_components)/'+imgName+'.png');
+              }
+              else {
+                mainGlob.push('**/'+libName+'/**/'+imgName+'.png');
+              }
+            }
+          }
+        }
+      }
+
+      //Use the specific glob to create a "child" gulp stream
+      //`stream` is referenced by means of closure is the parents gulp stream
+      //into which the matched image files are pushed
+      configStreams.imageApp(undefined, mainGlob)
+        .on('data', function(file) {
+          //Prefix the file name of all images with the project name so as to create
+          //disambiguation between sprites from mutliple dependencies.
+          //The need to manipulate file names is inherent from the spriting library
+          //using file names to generate CSS classes for each sprite.
+          var imageProjectName = projectOfFileInstance.name(file.path);
+          file.path = path.join(
+              process.cwd(), 'sprite', imageProjectName + '--' + path.basename(file.path));
+          stream.push(file);
+        })
+        .on('end', function() {
+          done();
+        });
     }))
-    .on('data', function(file) {
-    })
-    .on('end', function() {
-    })
     .pipe(
+      //finally invoke the actual spriting mechanism
       cssSprite.stream({
         name: 'sprite',
         style: '_sprite.scss',
